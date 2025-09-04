e543fe7fc149694aa15d7f132d110184956621ce
Samrat34 <mehedihasansamrat.mhs01@gmail.com>
2025-09-04 16:52:18 +0600

    edit Main. dart page

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,11 +1,16 @@
-import 'package:sales_force_app/views/constants/app_bindings.dart';
-import 'package:sales_force_app/views/outlets/outlets_screen.dart';
-import 'package:sales_force_app/views/constants/preloader_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:sales_force_app/controllers/user_controller.dart';
+import 'package:sales_force_app/views/constants/app_bindings.dart';
+import 'package:sales_force_app/views/constants/preloader_screen.dart';
+import 'package:sales_force_app/views/admin/dashboard/super_admin_dashboard.dart';
+import 'package:sales_force_app/views/outlets/outlets_screen.dart';
 
 void main() {
-  runApp(const MyApp());
+  // Initialize controllers here
+  Get.put(UserController());
+
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -15,29 +20,19 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return GetMaterialApp(
       debugShowCheckedModeBanner: false,
-
       title: 'SalesForce CRM',
-      initialBinding:
-          AppBinding(), // Add this line to register your controllers
       initialRoute: '/',
+      initialBinding: AppBinding(), // 👈 ensure bindings are loaded
       getPages: [
         GetPage(name: '/', page: () => PreloaderScreen()),
         GetPage(
-          name: '/outlets',
-          page: () => OutletScreen(),
-          // Optional: You can also add binding here if you want route-specific binding
-          // binding: BindingsBuilder(() {
-          //   Get.lazyPut<OutletController>(() => OutletController());
-          // }),
-        ),
+            name: '/superadmin-dashboard', page: () => SuperAdminDashboard()),
+        GetPage(name: '/outlets', page: () => OutletScreen()),
       ],
-      // Fallback for unknown routes
       unknownRoute: GetPage(
         name: '/notfound',
         page: () => Scaffold(
-          body: Center(
-            child: Text('Page not found'),
-          ),
+          body: Center(child: Text('Page not found')),
         ),
       ),
     );
